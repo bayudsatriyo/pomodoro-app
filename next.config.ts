9419853d6317b88bc45cec18219a27972ab3e696
bayudsatriyo <bayudsatriyo@gmail.com>
2026-01-19 09:27:42 +0700

    `fix(config)`: Remove Turbopack workaround

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Disable Turbopack (buggy in 16.1.1)
-  experimental: {
-    turbo: undefined,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
